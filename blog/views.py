fd5e804b54342c2f0c75e5e7dc9e38f5f8f584fe
Aditya Kharadkar <adityakharadkar27@gmail.com>
2022-09-22 22:01:24 +0530

    Served two templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts =[
+	{
+		"Author": "Aditya Kharadkar",
+		"Title": "Python",
+		"Date": "August 27, 2022",
+		"Content": "This is an article or post about Python."
+	},
+	{
+		"Author": "Prasad Kulkarni",
+		"Title": "Account",
+		"Date": "August 28, 2022",
+		"Content": "This is an article or post about Accounts."
+	},
+]
+
 # Create your views here.
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title': 'About'})
