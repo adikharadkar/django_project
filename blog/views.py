7a000c5a9782e0dcb40c703d385af1731a727148
Aditya Kharadkar <adityakharadkar27@gmail.com>
2022-09-24 17:01:57 +0530

    deleted dummy post data from views.py

--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
 
-posts =[
-	{
-		"author": "Aditya Kharadkar",
-		"title": "Python",
-		"date_posted": "August 27, 2022",
-		"content": "This is an article or post about Python."
-	},
-	{
-		"author": "Prasad Kulkarni",
-		"title": "Account",
-		"date_posted": "August 28, 2022",
-		"content": "This is an article or post about Accounts."
-	},
-]
-
 # Create your views here.
 def home(request):
 	context = {
